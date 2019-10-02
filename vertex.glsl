270faf758f9f06d3483b392b05fc9c463f022196
Fandi PJ <37364776+fandipj98@users.noreply.github.com>
2019-10-02 14:56:56 +0700

    animasi final

--- vertex.glsl
+++ vertex.glsl
@@ -12,8 +12,8 @@ uniform int flag;
 
 void main() {
   fColor = vColor;
-  vec2 pusatKiri = vec2(-0.5, 0.0);
-  vec2 pusatKanan = vec2(0.5, 0.0);
+  vec3 pusatKiri = vec3(-0.5, 0.0, 0.0);
+  vec3 pusatKanan = vec3(0.5, 0.0, 0.0);
 
   mat4 matrixRotate = mat4(
     cos(theta), sin(theta), 0.0, 0.0,
@@ -33,14 +33,14 @@ void main() {
     1.0, 0.0, 0.0, 0.0,
     0.0, 1.0, 0.0, 0.0,
     0.0, 0.0, 1.0, 0.0,
-    pusatKiri, 0.0, 1.0
+    pusatKiri, 1.0
   );
 
   mat4 matrixTranslationRight = mat4(
     1.0, 0.0, 0.0, 0.0,
     0.0, 1.0, 0.0, 0.0,
     0.0, 0.0, 1.0, 0.0,
-    pusatKanan, 0.0, 1.0
+    pusatKanan, 1.0
   );
 
   if(flag == 0){
